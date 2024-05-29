15938db7a4ec1e5d6406e41ae00f4537392c5b8b
DanielDubon <dub22233@uvg.edu.gt>
2024-05-28 21:38:03 -0600

    feat: contact component done

--- Portafolio-Dubon/src/components/Contact.jsx
+++ Portafolio-Dubon/src/components/Contact.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import './Contact.css';
+import iconImage from '../assets/contact-icon.png';
+
 
 const Contact = () => {
     return (
-        <section id="contact">
-            <h2>Contacto</h2>
-            <ul>
-                <li>Correo: danieldubob@gmail.com</li>
-                <li>Numero: +502 59345311</li>
-            </ul>
+        <section id="contact" className="contact-section">
+            <div className="contact-icon-container">
+                <img src={iconImage} alt="Icon" className="contact-icon" />
+            </div>
+            <div className="contact-container">
+                <h2>Contacto</h2>
+                <ul className="contact-list">
+                    <li>Correo: <a href="mailto:danieldubob@gmail.com">danieldubob@gmail.com</a></li>
+                    <li>Número: <a href="tel:+50259345311">+502 59345311</a></li>
+                </ul>
+            </div>
         </section>
     );
 };
