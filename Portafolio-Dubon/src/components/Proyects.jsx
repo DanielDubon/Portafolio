4e82c4976226cbeca56f40d24d677ff4f68a7a40
DanielDubon <dub22233@uvg.edu.gt>
2024-05-28 21:19:10 -0600

    feat: proyects section was finished

--- Portafolio-Dubon/src/components/Proyects.jsx
+++ Portafolio-Dubon/src/components/Proyects.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
+import iconImage from '../assets/proyects.png';
+import './Proyects.css';
+
 
 const projects = [
     {
-        title: 'Proyecto 1',
-        description: 'Descripción breve del proyecto 1.',
-        link: '#'
+        title: 'Sanitas',
+        description: 'Software para una Clinica',
+        link: 'https://github.com/SanitasUVG/Sanitas.git'
     },
     {
-        title: 'Proyecto 2',
-        description: 'Descripción breve del proyecto 2.',
-        link: '#'
+        title: 'Evolve',
+        description: 'Aplicacion fitness movil',
+        link: 'https://github.com/DanielDubon/Evolve.git'
     },
+    {
+        title: 'Hell Divers Blog',
+        description: 'Blog Web',
+        link: 'https://github.com/DanielDubon/HellDiversBlog_ViteReact.git'
+    },
+
 
 ];
 
 const Projects = () => {
     return (
-        <section id="projects">
-            <h2>Proyectos</h2>
-            <p>Lista de tus proyectos.</p>
+
+        <section id="projects" className="projects-card-main">
+            <div className="project-icon-container">
+                <img src={iconImage} alt="Icon" className="project-icon" />
+            </div>
+            <div className="projects-list">
+                <h2>Proyectos</h2>
+                {projects.map((project, index) => (
+                    <div key={index} className="project-card">
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <a href={project.link} className="project-link">Ver más</a>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 };
